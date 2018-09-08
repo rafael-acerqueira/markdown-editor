8c21eea0d7b8f4bee73dad8b602820d7b3a5cd80
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-09-08 16:26:36 -0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,42 +1,15 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React from 'react'
 
 import './css/style.css'
 
-class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
+const App = () => (
+  <div className='editor'>
+    <textarea />
+    <div className='view' />
+  </div>
+)
 
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    const title = await import('components/title')
-
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })
-  }
-
-  render () {
-    return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
-      </div>
-    )
-  }
-}
 
 export default App
