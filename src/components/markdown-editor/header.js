017babf3504bfc04eac4c0fc92112533314d3b95
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-09-19 07:15:35 -0300

    Refactor save message

--- src/components/markdown-editor/header.js
+++ src/components/markdown-editor/header.js
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types'
 
 const MarkdownEditorHeader = ({ isSaving, handleRemove, handleCreate }) => (
   <header className='header'>
-    <p className='save-message'>{isSaving ? 'Salvando...' : 'Salvo!'}</p>
+    {isSaving !== null && (
+      <p className='save-message'>
+          {isSaving ? 'Salvando...' : 'Salvo!'}
+      </p>
+    )}
+
     <button onClick={handleCreate}>Criar novo</button>
     <button onClick={handleRemove} className='remove-button'>Remover</button>
   </header>
 )
 
 MarkdownEditorHeader.propTypes = {
-  isSaving: PropTypes.bool.isRequired,
+  isSaving: PropTypes.bool,
   handleRemove: PropTypes.func.isRequired,
   handleCreate: PropTypes.func.isRequired
 }
